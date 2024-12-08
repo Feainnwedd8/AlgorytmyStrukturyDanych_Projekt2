0fb3d6eab2e8b186cafc395cbbfcf7503737dc49
Feainnwedd8 <sobkowiak.krzysztof@outlook.com>
2024-12-08 21:58:51 +0100

    Dodaj pliki projektów.

---
+++ AlgorytmyStrukturyDanych_Projekt2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int n = 100000; n == 1000000; n += 100000)
+        {
+
+            Random rn = new Random();
+            int[] arr = new int[n];
+
+
+        }
+        
+
+    }
+
+    static void SelectionSort(int[] arr)
+    {
+        int n = arr.Length;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++) if (arr[j] < arr[min_idx]) min_idx = j;
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        int n = arr.Length;
+
+        for (int i = 0; i < n; ++i)
+            Console.Write(arr[i] + " ");
+
+        Console.WriteLine();
+    }
+}
