aa6590a9d7665124b90ac0800a6e6d5f943eed8e
Feainnwedd8 <sobkowiak.krzysztof@outlook.com>
2024-12-25 21:41:46 +0100

    coss nie dziala ale niby wszystko

--- AlgorytmyStrukturyDanych_Projekt2/Program.cs
+++ AlgorytmyStrukturyDanych_Projekt2/Program.cs
@@ -8,25 +8,38 @@ wywolywać funkcje, ktora od razu zrobi te 10 pomiarów, policzy srednie i wypis
 NIECH TO BEDA FUNKCJE KORZYSTAJACE Z OSOBNO ZROBIONYCH FUNKCJI SORTUJACYCH
 TZN FUNKCJA KTORA PO 10 RAZY WYWOLUJE SORT_XXXX, LICZY ITD
 
-MOZE TO Z JEDNEJ TABLICY WARTOSCI ZROBIC TE KILKA RODZAJOW? 
+MOZE TO Z JEDNEJ TABLICY WARTOSCI ZROBIC TE KILKA RODZAJOW? ------
   
   
   */
 
 
 using System;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Linq;
+
+
 
 class Program
 {
+    
+
     static void Main(string[] args)
     {
-        for (int n = 100000; n == 1000000; n += 100000)
+        for (int n = 100_000; n == 1_000_000; n += 100_000)
         {
 
             Random rn = new Random();
             int[] arr = new int[n];
 
-            
+            for (int i = 0; i < n; i++) arr[i] = rn.Next(1, n);
+
+            RandomValuesArr(arr);
+            AscValuesArr(arr);
+            DescValuesArr(arr);
+            VshapeValuesArr(arr);
+            ConstValuesArr(n);
         }
         
 
@@ -46,13 +59,263 @@ class Program
         }
     }
 
-    static void PrintArray(int[] arr)
+    static void InsertionSort(int[] arr)
     {
-        int n = arr.Length;
+        for (int i = 1; i < arr.Length; ++i)
+        {
+            int current = arr[i];
+            int j = i - 1;
 
-        for (int i = 0; i < n; ++i)
-            Console.Write(arr[i] + " ");
+            while (j >= 0 && arr[j] > current)
+            {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
 
-        Console.WriteLine();
+            arr[j + 1] = current;
+        }
     }
+
+    public static void BubbleSort(int[] arr)
+    {
+        int temp;
+        bool swapped;
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            swapped = false;
+            for (int j = 0; j < arr.Length - 1 - i; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped)
+            {
+                break;
+            }
+        }
+    }
+
+    static void RandomValuesArr(int[] arr)
+    {
+        double[] Tsuma = new double[gIterations];
+        Console.WriteLine("\n");
+        Console.WriteLine("Tablica z losowym ustawieniem wartości");
+        Console.WriteLine("Średni czas wykonania dla: ");
+
+        for (int i = 0; i > gIterations; i++)
+        {
+            long StartingTime = Stopwatch.GetTimestamp();
+            SelectionSort(arr);
+            long EndingTime = Stopwatch.GetTimestamp();
+            long ElapsedTime = EndingTime - StartingTime;
+            Tsuma[i] = ElapsedTime * (1.0 / Stopwatch.Frequency);
+        }
+        double avg = Tsuma.Sum() / gIterations;
+        Console.WriteLine("Selection Sort: " + avg + " [s]");
+
+        for (int i = 0; i > gIterations; i++)
+        {
+            long StartingTime = Stopwatch.GetTimestamp();
+            InsertionSort(arr);
+            long EndingTime = Stopwatch.GetTimestamp();
+            long ElapsedTime = EndingTime - StartingTime;
+            Tsuma[i] = ElapsedTime * (1.0 / Stopwatch.Frequency);
+        }
+        avg = Tsuma.Sum() / gIterations;
+        Console.WriteLine("Insertion Sort: " + avg + " [s]");
+
+        for (int i = 0; i > gIterations; i++)
+        {
+            long StartingTime = Stopwatch.GetTimestamp();
+            BubbleSort(arr);
+            long EndingTime = Stopwatch.GetTimestamp();
+            long ElapsedTime = EndingTime - StartingTime;
+            Tsuma[i] = ElapsedTime * (1.0 / Stopwatch.Frequency);
+        }
+        avg = Tsuma.Sum() / gIterations;
+        Console.WriteLine("Bubble Sort: " + avg + " [s]");
+    }
+
+
+    static void AscValuesArr(int[] arr)
+    {
+        Array.Sort(arr);
+
+        double[] Tsuma = new double[gIterations];
+        Console.WriteLine("\n");
+        Console.WriteLine("Tablica z rosnącym ustawieniem wartości");
+        Console.WriteLine("Średni czas wykonania dla: ");
+
+        for (int i = 0; i > gIterations; i++)
+        {
+            long StartingTime = Stopwatch.GetTimestamp();
+            SelectionSort(arr);
+            long EndingTime = Stopwatch.GetTimestamp();
+            long ElapsedTime = EndingTime - StartingTime;
+            Tsuma[i] = ElapsedTime * (1.0 / Stopwatch.Frequency);
+        }
+        double avg = Tsuma.Sum() / gIterations;
+        Console.WriteLine("Selection Sort: " + avg + " [s]");
+
+        for (int i = 0; i > gIterations; i++)
+        {
+            long StartingTime = Stopwatch.GetTimestamp();
+            InsertionSort(arr);
+            long EndingTime = Stopwatch.GetTimestamp();
+            long ElapsedTime = EndingTime - StartingTime;
+            Tsuma[i] = ElapsedTime * (1.0 / Stopwatch.Frequency);
+        }
+        avg = Tsuma.Sum() / gIterations;
+        Console.WriteLine("Insertion Sort: " + avg + " [s]");
+
+        for (int i = 0; i > gIterations; i++)
+        {
+            long StartingTime = Stopwatch.GetTimestamp();
+            BubbleSort(arr);
+            long EndingTime = Stopwatch.GetTimestamp();
+            long ElapsedTime = EndingTime - StartingTime;
+            Tsuma[i] = ElapsedTime * (1.0 / Stopwatch.Frequency);
+        }
+        avg = Tsuma.Sum() / gIterations;
+        Console.WriteLine("Bubble Sort: " + avg + " [s]");
+    }
+
+    static void DescValuesArr(int[] arr)
+    {
+        Array.Sort(arr);
+        Array.Reverse(arr);
+
+        double[] Tsuma = new double[gIterations];
+        Console.WriteLine("\n");
+        Console.WriteLine("Tablica z malejącym ustawieniem wartości");
+        Console.WriteLine("Średni czas wykonania dla: ");
+
+        for (int i = 0; i > gIterations; i++)
+        {
+            long StartingTime = Stopwatch.GetTimestamp();
+            SelectionSort(arr);
+            long EndingTime = Stopwatch.GetTimestamp();
+            long ElapsedTime = EndingTime - StartingTime;
+            Tsuma[i] = ElapsedTime * (1.0 / Stopwatch.Frequency);
+        }
+        double avg = Tsuma.Sum() / gIterations;
+        Console.WriteLine("Selection Sort: " + avg + " [s]");
+
+        for (int i = 0; i > gIterations; i++)
+        {
+            long StartingTime = Stopwatch.GetTimestamp();
+            InsertionSort(arr);
+            long EndingTime = Stopwatch.GetTimestamp();
+            long ElapsedTime = EndingTime - StartingTime;
+            Tsuma[i] = ElapsedTime * (1.0 / Stopwatch.Frequency);
+        }
+        avg = Tsuma.Sum() / gIterations;
+        Console.WriteLine("Insertion Sort: " + avg + " [s]");
+
+        for (int i = 0; i > gIterations; i++)
+        {
+            long StartingTime = Stopwatch.GetTimestamp();
+            BubbleSort(arr);
+            long EndingTime = Stopwatch.GetTimestamp();
+            long ElapsedTime = EndingTime - StartingTime;
+            Tsuma[i] = ElapsedTime * (1.0 / Stopwatch.Frequency);
+        }
+        avg = Tsuma.Sum() / gIterations;
+        Console.WriteLine("Bubble Sort: " + avg + " [s]");
+    }
+
+    static void VshapeValuesArr(int[] arr)
+    {
+        Array.Sort(arr, 0, arr.Length / 2 + 1);
+        Array.Reverse(arr, 0, arr.Length / 2 + 1); 
+        Array.Sort(arr, arr.Length / 2, arr.Length / 2 + arr.Length % 2);
+
+        double[] Tsuma = new double[gIterations];
+        Console.WriteLine("\n");
+        Console.WriteLine("Tablica z  V-kształtnym ustawieniem wartości");
+        Console.WriteLine("Średni czas wykonania dla: ");
+
+        for (int i = 0; i > gIterations; i++)
+        {
+            long StartingTime = Stopwatch.GetTimestamp();
+            SelectionSort(arr);
+            long EndingTime = Stopwatch.GetTimestamp();
+            long ElapsedTime = EndingTime - StartingTime;
+            Tsuma[i] = ElapsedTime * (1.0 / Stopwatch.Frequency);
+        }
+        double avg = Tsuma.Sum() / gIterations;
+        Console.WriteLine("Selection Sort: " + avg + " [s]");
+
+        for (int i = 0; i > gIterations; i++)
+        {
+            long StartingTime = Stopwatch.GetTimestamp();
+            InsertionSort(arr);
+            long EndingTime = Stopwatch.GetTimestamp();
+            long ElapsedTime = EndingTime - StartingTime;
+            Tsuma[i] = ElapsedTime * (1.0 / Stopwatch.Frequency);
+        }
+        avg = Tsuma.Sum() / gIterations;
+        Console.WriteLine("Insertion Sort: " + avg + " [s]");
+
+        for (int i = 0; i > gIterations; i++)
+        {
+            long StartingTime = Stopwatch.GetTimestamp();
+            BubbleSort(arr);
+            long EndingTime = Stopwatch.GetTimestamp();
+            long ElapsedTime = EndingTime - StartingTime;
+            Tsuma[i] = ElapsedTime * (1.0 / Stopwatch.Frequency);
+        }
+        avg = Tsuma.Sum() / gIterations;
+        Console.WriteLine("Bubble Sort: " + avg + " [s]");
+    }
+
+    static void ConstValuesArr(int iteration)
+    {
+        int[] arr = new int[iteration];
+        for (int i=0; i < arr.Length; i++) arr[i] = 8;
+        double[] Tsuma = new double[gIterations];
+        Console.WriteLine("\n");
+        Console.WriteLine("Tablica ze stałymi wartościami");
+        Console.WriteLine("Średni czas wykonania dla: ");
+
+        for (int i = 0; i > gIterations; i++)
+        {
+            long StartingTime = Stopwatch.GetTimestamp();
+            SelectionSort(arr);
+            long EndingTime = Stopwatch.GetTimestamp();
+            long ElapsedTime = EndingTime - StartingTime;
+            Tsuma[i] = ElapsedTime * (1.0 / Stopwatch.Frequency);
+        }
+        double avg = Tsuma.Sum() / gIterations;
+        Console.WriteLine("Selection Sort: " + avg + " [s]");
+
+        for (int i = 0; i > gIterations; i++)
+        {
+            long StartingTime = Stopwatch.GetTimestamp();
+            InsertionSort(arr);
+            long EndingTime = Stopwatch.GetTimestamp();
+            long ElapsedTime = EndingTime - StartingTime;
+            Tsuma[i] = ElapsedTime * (1.0 / Stopwatch.Frequency);
+        }
+        avg = Tsuma.Sum() / gIterations;
+        Console.WriteLine("Insertion Sort: " + avg + " [s]");
+
+        for (int i = 0; i > gIterations; i++)
+        {
+            long StartingTime = Stopwatch.GetTimestamp();
+            BubbleSort(arr);
+            long EndingTime = Stopwatch.GetTimestamp();
+            long ElapsedTime = EndingTime - StartingTime;
+            Tsuma[i] = ElapsedTime * (1.0 / Stopwatch.Frequency);
+        }
+        avg = Tsuma.Sum() / gIterations;
+        Console.WriteLine("Bubble Sort: " + avg + " [s]");
+    }
+    
+        const int gIterations = 10;    
 }
+
